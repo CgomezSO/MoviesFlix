b845d4decbbb15e8c57d0a7461b2fbd3cbb2eeb5
CarlosGomezz <carlosgomezfuentes2003@gmail.com>
2025-04-29 00:16:56 +0200

    Expo

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,69 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+    ActivityIndicator,
+    FlatList,
+    Image,
+    StyleSheet,
+    Text,
+    View,
+} from "react-native";
 import React from "react";
+import { images } from "@/constants/images";
+import { useRouter } from "expo-router";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import MovieCard from "../components/MovieCard";
+import { icons } from "@/constants/icons";
+import SearchBar from "../components/SearchBar";
+// import { SearchBar } from "@/components/SearchBar";
 
 const Search = () => {
+    const router = useRouter();
+
+    const {
+        data: movies,
+        loading: moviesLoading,
+        error: moviesError,
+    } = useFetch(() => fetchMovies({ query: "" }));
+
     return (
-        <View>
-            <Text>Search</Text>
+        <View className="flex-1 bg-primary">
+            <Image
+                source={images.bg}
+                className="flex-1 absolute w-full z-0"
+                resizeMode="cover"
+            />
+
+            <FlatList
+                data={movies}
+                renderItem={({ item }) => <MovieCard {...item} />}
+                keyExtractor={(item) => item.id.toString()}
+                numColumns={3}
+                columnWrapperStyle={{
+                    justifyContent: "center",
+                    gap: 16,
+                    marginVertical: 16,
+                }}
+                contentContainerStyle={{ paddingBottom: 100 }}
+                ListHeaderComponent={
+                    <>
+                        <View className="w-full flex-row justify-center mt-20 items-center">
+                            <Image source={icons.logo} className="w-12 h-10" />
+                        </View>
+
+                        <View className="my-5">
+                            <SearchBar placeholder="Search movies ..." />
+                        </View>
+
+                        {moviesLoading && (
+                            <ActivityIndicator
+                                size="large"
+                                color="#0000ff"
+                                className="my-3"
+                            />
+                        )}
+                    </>
+                }
+            />
         </View>
     );
 };
